316e1723ac0d3eed7e7a60f643a67c6019c94f28
Igor P-V <ispogiba@gmail.com>
2021-02-11 19:30:33 +0300

    Initial Commit

---
+++ TestTaskPhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskPhotoApp
+//
+//  Created by Elena Igumenova on 11.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
